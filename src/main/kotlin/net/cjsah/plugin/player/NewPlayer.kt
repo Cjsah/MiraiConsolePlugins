5860f78f0e77c1bc10c9e2a6ab7799ccbbb99cf1
Cjsah <2684117397@qq.com>
2021-09-30 11:15:43 +0800

    5.0.0

--- src/main/kotlin/net/cjsah/plugin/player/NewPlayer.kt
+++ src/main/kotlin/net/cjsah/plugin/player/NewPlayer.kt
@@ -1,40 +1,46 @@
-package net.cjsah.bot.plugin.player
+package net.cjsah.plugin.player
 
+import cc.moecraft.yaml.HyConfig
 import kotlinx.coroutines.delay
-import net.cjsah.bot.console.Plugin
+import net.cjsah.console.Util
+import net.cjsah.console.plugin.Plugin
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.MemberJoinEvent
 import net.mamoe.mirai.message.data.At
+import java.io.File
 
 @Suppress("unused")
-class NewPlayer : Plugin(
-    "NewPlayer",
-    "4.2",
-    true,
-    listOf("Cjsah")
-) {
+class NewPlayer : Plugin() {
+    private lateinit var config: HyConfig
 
-    override suspend fun onPluginStart() {
-        val config = getYamlConfig("config.yml") { config ->
-            run {
-                config.set("群号", listOf(123))
-                config.set("内容", "文本")
-            }
+    override fun onPluginLoad() {
+        config = Util.getYaml(File(pluginDir, "config.yml")) {
+            it.set("群号", listOf(123))
+            it.set("内容", "文本")
         }
+    }
 
+    override fun onBotStarted() {
         GlobalEventChannel.subscribeAlways<MemberJoinEvent> {
             if (groupId in config.getLongList("群号")) {
                 val message = config.getString("内容").split("[part]")
                 for ((i,str) in message.withIndex()) {
-                    delay(500)
                     if (i == 0) {
                         group.sendMessage(At(this.member) + str)
                     } else {
                         group.sendMessage(str)
                     }
+                    delay(500)
                 }
             }
         }
+
+    }
+
+    override fun onBotStopped() {
+    }
+
+    override fun onPluginUnload() {
     }
 }
 
